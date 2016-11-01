7f9bbc8e6c9dc5a522ccac44d7f14317357750dc
CalvinMogodi <Calvin.Mogodi@eoh.co.za>
2016-11-02 01:08:29 +0200

    a

--- src/E Learning/wwwroot/app/components/assignment/assignmentAddEdit/assignmentAddEdit.controller.js
+++ src/E Learning/wwwroot/app/components/assignment/assignmentAddEdit/assignmentAddEdit.controller.js
@@ -1,19 +1,87 @@
 ﻿(function () {
     'use strict';
 
-    angular
-        .module('EL')
-        .controller('AssignmentAddEditController', AssignmentAddEditController);
-
-    AssignmentAddEditController.$inject = ['$location']; 
-
-    function AssignmentAddEditController($location) {
+    function AssignmentAddEditController($location, firebaseUrl, HelperService, $firebaseArray, $filter, $firebaseObject) {
         /* jshint validthis:true */
         var vm = this;
-        vm.title = 'AssignmentAddEditController';
+        var ref = new Firebase(firebaseUrl);
+        vm.isEdit = false;
 
-        activate();
+        init();
+        function init() {
+            vm.assignment = HelperService.getAssignedRecord();
+            vm.classes = $firebaseArray(ref.child('Class'));
+            vm.heading = 'Add New Assignment';
+            if (vm.assignment) {
+                vm.isEdit = true;
+                vm.heading = 'Update Assignment';
+                vm.assignment.class = vm.assignment.class;
+                vm.assignment.date = new Date(vm.assignment.date);
+            }
 
-        function activate() { }
+        }
+
+        vm.create = function (assignment) {
+
+            vm.formSubmitted = true;
+
+            if (vm.assignmentForm.$valid) {
+              
+                var f = document.getElementById('file').files[0],
+                r = new FileReader();
+                r.onloadend = function (e) {
+                    var data = e.target.result;
+                    //send your binary data via $http or $resource or do anything else with it
+
+                    var obj = JSON.parse(assignment.class);
+                    var assignmentRef = new Firebase(firebaseUrl + "/Assignment");
+                    var assignments = $firebaseArray(assignmentRef);
+
+                    assignment.date = $filter('date')(new Date(assignment.date), 'yyyy-MM-dd');
+                    var newRecord = {
+                        title: assignment.title,
+                        description: assignment.description,
+                        date: assignment.date,
+                        file: data,
+                        classId: obj.$id,
+                    };
+                    assignments.$add(newRecord);
+                    $location.path('/assignment');
+
+                }
+                r.readAsDataURL(f);
+
+               
+
+                
+            }
+        }
+
+        vm.update = function (assignment) {
+            vm.formSubmitted = true;
+
+            if (vm.assignmentForm.$valid) {
+                var obj = JSON.parse(assignment.class);
+                var editRef = new Firebase(firebaseUrl + "/Assignment/" + assignment.$id);
+                assignment.date = $filter('date')(new Date(assignment.date), 'yyyy-MM-dd');
+                var oldAssignmentt = $firebaseObject(editRef);
+
+                oldAssignmentt.$id = assignment.$id;
+                oldAssignmentt.description = assignment.description;
+                oldAssignmentt.title = assignment.title;
+                oldAssignmentt.date = assignment.date;
+                oldAssignmentt.classId = obj.$id;
+
+                oldAssignmentt.$save();
+                $location.path('/assignment');
+            }
+        }
+
+        vm.cancel = function () {
+            $location.path('/assignment');
+        }
     }
+
+    angular.module('EL').controller('AssignmentAddEditController', AssignmentAddEditController);
+    AssignmentAddEditController.$inject = ['$location', 'firebaseUrl', 'HelperService', '$firebaseArray', '$filter', '$firebaseObject'];
 })();
--- src/E Learning/wwwroot/app/components/assignment/assignmentView.controller.js
+++ src/E Learning/wwwroot/app/components/assignment/assignmentView.controller.js
@@ -1,19 +1,74 @@
 ﻿(function () {
     'use strict';
 
-    angular
-        .module('EL')
-        .controller('AssignmentController', AssignmentController);
-
-    AssignmentController.$inject = ['$location']; 
-
-    function AssignmentController($location) {
+    function AssignmentController($location, $firebaseArray, HelperService, alertDialogService, modal, firebaseUrl) {
         /* jshint validthis:true */
         var vm = this;
-        vm.title = 'AssignmentController';
+        vm.heading = 'Assignment';
+        vm.icon = "add_box";
+        var ref = new Firebase(firebaseUrl);
+        vm.pagenation = {
+            limit: 5,
+            page: 1,
+        };
 
-        activate();
+        init();
 
-        function activate() { }
+        function init() {
+            //load assignments with class that is linked to
+            vm.assignments = $firebaseArray(ref.child('Assignment'));
+            vm.assignments.$loaded(function (data) {
+                vm.classes = $firebaseArray(ref.child('Class'));
+                vm.classes.$loaded(function (data) {
+                    for (var i = 0; i < vm.assignments.length; i++) {
+                        for (var j = 0; j < vm.classes.length; j++) {
+                            if (vm.assignments[i].classId == vm.classes[j].$id) {
+                                vm.assignments[i].class = vm.classes[j];
+                                break;
+                            }
+                        }
+                    }
+                });
+            });
+
+        }
+
+        vm.newAssignment = function () {
+            HelperService.assignCurrentRecord(undefined);
+            $location.path('/assignmentAddEdit');
+        }
+        vm.editAssignment = function (assignment) {
+            HelperService.assignCurrentRecord(assignment);
+            $location.path('/assignmentAddEdit');
+        }
+
+        vm.downloadAssignment = function (assignment) {
+            //TODO
+            var xhr = new XMLHttpRequest();
+            xhr.open("GET", assignment.file);
+            xhr.responseType = "arraybuffer";
+
+            xhr.onload = function () {
+                if (this.status === 200) {
+                    var blob = new Blob([xhr.response], { type: "application/pdf" });
+                    var objectUrl = URL.createObjectURL(blob);
+                    window.open(objectUrl);
+                }
+            };
+            xhr.send();
+        }
+
+        vm.deleteAssignment = function (assignment) {
+            alertDialogService.setHeaderAndMessage('Delete', 'Are you sore you want to delete this class?');
+            var templateUrl = '/app/common/alert/alertDialog.template.html';
+            modal.show(templateUrl, 'alertDialogController').then(function (result) {
+                if (result) {
+                    vm.assignments.$remove(assignment);
+                }
+            });
+        }
     }
+
+    angular.module('EL').controller('AssignmentController', AssignmentController);
+    AssignmentController.$inject = ['$location', '$firebaseArray', 'HelperService', 'alertDialogService', 'modal', 'firebaseUrl'];
 })();
